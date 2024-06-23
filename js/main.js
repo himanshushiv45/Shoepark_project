8e30fbb26852a236b33fb11f2b3182f0c47845f2
Himanshu Shiv <mail.himanshushiv@gmail.com>
2024-06-23 13:49:55 +0530

    Filter Project

---
+++ js/main.js
@@ -0,0 +1,301 @@
+let productData = [
+    {
+        id: 1,
+        name: "Combo Pack Of 2 Running Shoes For Men (Black)",
+        desc: "MOST SEARCHED IN Footwear:US POLO INDIAF SPORTS SHOESWOODLAND FOOTWEARBOOTSLOAFER SHOESWHITE SHOES FOR MENWHITE SNEAKERSYEPME FORMAL SHOESRED CHIEF LEATHER SHOESREEBOK SHOES PRICE LISTADIDAS FLOATERSBEST SANDALS FOR MEN",
+        size: [5, 6, 7, 8, 9],
+        color: ["white"],
+        price: 531,
+        rating: 3.6,
+        brand: "SPORT FASSION",
+        imgURL: "https://rukminim2.flixcart.com/image/612/612/xif0q/shoe/j/a/h/6-rsl056-red-tape-off-white-blue-original-imagyua3dxnh4arf.jpeg?q=70"
+    },
+    {
+        id: 2,
+        name: "Trending Stylish Casual Outdoor Shoes Sneakers For Men  (White, Black)",
+        desc: "MOST SEARCHED IN Footwear:US POLO INDIAF SPORTS SHOESWOODLAND FOOTWEARBOOTSLOAFER SHOESWHITE SHOES FOR MENWHITE SNEAKERSYEPME FORMAL SHOESRED CHIEF LEATHER SHOESREEBOK SHOES PRICE LISTADIDAS FLOATERSBEST SANDALS FOR MEN",
+        size: [5, 6, 7, 8, 9],
+        color: ["white", "Black"],
+        price: 299,
+        rating: 3.8,
+        brand: "URBANBOX",
+        imgURL: "https://rukminim2.flixcart.com/image/612/612/xif0q/shoe/x/c/p/10-rsl030-red-tape-black-red-original-imagthjckfw5tpby.jpeg?q=70"
+    },
+    {
+        id: 3,
+        name: "Trendy & Stylish Running Shoes For Men  (Blue, red)",
+        desc: "MOST SEARCHED IN Footwear:LAKME LIPSTICK SHADES WITH PRICEFOOTWEAR BRANDSDC SHOES INDIAACTION SHOES ONLINEREEBOK CASUAL SHOESUNITED COLORS OF BENETTON SHOESYEPME SHOES 499LEE COOPER SHOESRED CHIEF SHOES ONLINEREEBOK SANDALSSPARX SANDALS ONLINE",
+        size: [5, 6, 7, 8, 9],
+        color: ["white","black"],
+        price: 284,
+        rating: 3.7,
+        brand: "HOTSTYLE",
+        imgURL: "https://rukminim2.flixcart.com/image/612/612/xif0q/shoe/l/m/y/8-bd-1191wh-8-nivia-white-original-imagndzcuhvmuegz.jpeg?q=70"
+    },
+    {
+        id: 4,
+        name: "Casual Sneaker Shoes For Men | Enhanced Comfort with Cushioned Insole Sneakers For Men  (White, Blue)",
+        desc: "MOST SEARCHED IN Footwear:ADIDAS STUDSMOCHI SHOES ONLINEPARAGON FOOTWEAR ONLINESLAZENGERSUPRA SHOES INDIAHIP HOP SHOESADIDAS TRACKSUITBATA CASUAL SHOESCROCS SANDALSLEATHER SHOESREDTAPEADIDAS SANDALS FOR MENNIKE SLIPPERS ONLINESPARXADIDAS RUNNING SHOESNIVIAPUMA RUNNING SHOESPUMA BMW SHOES",
+        size: [5, 6, 7, 8, 9],
+        color: ["white", "blue"],
+        price: 1679,
+        rating: 4.3,
+        brand: "SPORT FASSION",
+        imgURL: "https://rukminim2.flixcart.com/image/832/832/xif0q/shoe/9/y/8/9-rsl027-red-tape-white-blue-original-imagszhhyk7nndwg.jpeg?q=70&crop=false"
+    },
+    {
+        id: 5,
+        name: "Genuine Leather Ankle Length Boots for Men | Soft Cushioned Insole Boots For Men  (white,Black)",
+        desc: "MOST SEARCHED IN Footwear:KIDS SLIPPERSVANS SHOES ONLINE INDIANON MARKING SHOESBOOTSSNEAKERS ONLINECROCS FOR MENBLACK SHOESGENTS SHOESPUMA SHOSELEATHER SANDALSWOODLANDS SANDALSYEPME SANDALSADIDAS SLIPPERSNIKE SLIPPER",
+        size: [5, 6, 7, 8, 9],
+        color: ["blue","black"],
+        price: 1859,
+        rating: 4.1,
+        brand: "HOTSTYLE",
+        imgURL: "https://rukminim2.flixcart.com/image/612/612/xif0q/shoe/w/k/g/7-6g-842-7-campus-bt-grn-blk-original-imagsfs46jdxt5cb.jpeg?q=70"
+    },
+    {
+        id: 6,
+        name: "Coy Float Slip On Sneakers For Men  (red)",
+        desc: "MOST SEARCHED IN Footwear:CONVERSE SNEAKERSKIDS SPORTS SHOESLAKHANI TOUCH SHOESSNEAKERS SHOES FOR MENFILASEMI FORMAL SHOESLANCER SHOESLEE COOPER SHOES ONLINELI NING BADMINTON SHOESPUMA SHOES ONLINE SHOPPINGRED CHIEF SHOES PRICE LISTBEST SANDALS FOR MEN",
+        size: [5, 6, 7, 8, 9],
+        color: ["red","black"],
+        price: 1626,
+        rating: 4.2,
+        brand: "PUMA",
+        imgURL: "https://rukminim2.flixcart.com/image/612/612/xif0q/shoe/6/n/y/9-rso375-red-tape-white-original-imagyt893hzmjacr.jpeg?q=70"
+    },
+    {
+        id: 7,
+        name: "Wired Rapid Sneakers For Men  (White)",
+        desc: "MOST SEARCHED IN Footwear:LUSH INDIACATWALKCHILDREN SHOESUNISTAR SHOESVOLTAS INDIAALBERTO TORRESICASUAL SHOES MENPUMA CASUAL SHOESCROCS FOOTWEARBUY SHOES ONLINE INDIAPUMA SHOES FOR MENRED CHIEF SHOES ONLINEREEBOK SHOES PRICE LIST",
+        size: [5, 6, 7, 8, 9],
+        color: ["white","red"],
+        price: 2640,
+        rating: 4.2,
+        brand: "HOTSTYLE",
+        imgURL: "https://rukminim2.flixcart.com/image/612/612/xif0q/shoe/6/2/x/-original-imagy2h6fdugwg37.jpeg?q=70"
+    },
+    {
+        id: 8,
+        name: "Voyager M Running Shoes For Men  (blue)",
+        desc: "MOST SEARCHED IN Footwear:LP SHOESALLEN SOLLY SHOESADIDAS CANVAS SHOESMENS LOAFERSNIKE CASUAL SHOESVANS CASUAL SHOESLEE COOPER SHOES INDIALI NING BADMINTON SHOESNIKE SHOES WITH PRICEMEN FOOTWEARSHOES PUMAREDTAPEREEBOK SHOES MODELS WITH PRICE",
+        size: [5, 6, 7, 8, 9],
+        color: ["white"],
+        price: 1302,
+        rating: 4.2,
+        brand: "SPORT FASSION",
+        imgURL: "https://rukminim2.flixcart.com/image/612/612/xif0q/shoe/q/m/t/12-380190-puma-white-cool-light-grey-original-imagyfgphnphd6av.jpeg?q=70"
+    },
+    {
+        id: 9,
+        name: "HURRICANE Running Shoes For Men  (Blue)",
+        desc: "MOST SEARCHED IN Footwear:CONVERSE SHOES ONLINEKIDS SHOESLIBERTY SHOESYEP ME.COM SHOESHIGH ANKLE SHOESDIESEL JEANS PRICEBEST LOAFERS FOR MENBEST CASUAL SHOES FOR MENSEMI FORMAL SHOESPUMA SHOES ONLINE SHOPPINGRED CHIEF SHOES WITH PRICEADIDAS FLOATERS",
+        size: [5, 6, 7, 8, 9],
+        color: ["black","blue"],
+        price: 699,
+        rating: 4.3,
+        brand: "PUMA",
+        imgURL: "https://rukminim2.flixcart.com/image/612/612/xif0q/shoe/o/m/y/-original-imagzry3gugpvqty.jpeg?q=70"
+    },
+    {
+        id: 10,
+        name: "Casual Sneaker Shoes for Men |Best Shoes For Men  (Grey, Black, White)",
+        desc: "MOST SEARCHED IN Footwear:UNITED COLORS OF BENETTON ONLINEADIDAS NEO SHOESSLAZENGER SHOESREEBOK TRACKSUITACTION SHOES ONLINEBATA SANDALS FOR MENSBUY BOOTS ONLINE INDIANIKE CASUAL SHOES FOR MENWOODLAND CASUAL FORMAL SHOES",
+        size: [5, 6, 7, 8, 9],
+        color: ["white","black"],
+        price: 1119,
+        rating: 4.1,
+        brand: "URBANBOX",
+        imgURL: "https://rukminim2.flixcart.com/image/612/612/xif0q/shoe/w/f/2/-original-imagrzqaafqyya4a.jpeg?q=70"
+    }
+]
+
+
+// filter-Modal🔥
+let status = false;
+function filter() {
+
+    if (status === false) {
+        let filterModal = document.getElementById("filterModal");
+        filterModal.style.left = "0px";
+        status = true;
+    } else {
+        let filterModal = document.getElementById("filterModal");
+        filterModal.style.left = "-380px";
+        status = false;
+    }
+}
+
+
+
+// function for create dinamic card🔥
+function displayData(arr) {
+    document.getElementById("card_container").innerHTML = "";
+
+    arr.forEach((element, index) => {
+        // console.log(element)
+
+        let card_container = document.getElementById("card_container");
+
+        let item = document.createElement("div");
+        item.setAttribute("class", "item");
+
+        let card_img = document.createElement("div");
+        card_img.setAttribute("class", "card_img");
+
+        let img = document.createElement("img");
+        img.setAttribute("src", element.imgURL)
+        card_img.appendChild(img);
+
+        let card_info = document.createElement("div");
+        card_info.setAttribute("class", "card_info");
+
+        let p1 = document.createElement("p");
+        p1.setAttribute("class", "sponsored");
+        p1.append("Sponsored")
+        card_info.appendChild(p1);
+
+        let p2 = document.createElement("p");
+        p2.setAttribute("class", "brand_name");
+        p2.append(element.brand)
+        card_info.appendChild(p2);
+
+        let p3 = document.createElement("p");
+        p3.setAttribute("class", "product_info");
+        p3.append(element.name)
+        card_info.appendChild(p3);
+
+
+        let price_container = document.createElement("div");
+        price_container.setAttribute("class", "price_container");
+
+        let span1 = document.createElement("span");
+        span1.setAttribute("class", "product_price");
+        span1.innerHTML = `&#8377;${element.price}`;
+        price_container.appendChild(span1)
+
+        let span2 = document.createElement("span");
+        span2.setAttribute("class", "real_price");
+        span2.innerHTML = `&#8377;${2449}`;
+        price_container.appendChild(span2)
+
+        let span3 = document.createElement("span");
+        span3.setAttribute("class", "product_discount");
+        span3.append("80% off");
+        price_container.appendChild(span3)
+
+
+        let span4 = document.createElement("span");
+        span4.setAttribute("class", "deal");
+        span4.append("Hot Deal");
+
+        card_info.appendChild(price_container);
+        card_info.appendChild(span4);
+
+        item.appendChild(card_img);
+        item.appendChild(card_info)
+
+        card_container.appendChild(item);
+    });
+
+}
+displayData(productData);
+
+
+
+
+// function for set values in OBJ🔥
+let filterObj = {
+    name: null,
+    minPrice: null,
+    maxPrice: null,
+    brand: null,
+    size: null,
+    color: null
+}
+
+
+function setValue(property, value) {
+    if (value.trim() === ' ') {
+        filterObj[property] = null;
+
+    } else {
+        filterObj[property] = value;  //set property👍
+    }
+}
+
+
+function getCheckedColor() {
+    let checkItem = document.querySelectorAll('input[type = "checkbox"]')
+
+    let colorCollection = [];
+
+    checkItem.forEach((element, index) => {
+        if (element.checked === true) {
+            colorCollection.push(element.name)
+            // console.log(colorCollection)
+        }
+    })
+
+    if (colorCollection.value !== 0) {
+        filterObj.color = colorCollection
+
+    } else {
+        filterObj.color = null
+    }
+}
+
+
+
+
+
+
+const filterData = () => {
+
+    let filterArr = JSON.parse(JSON.stringify(productData));
+
+    // check name
+    if (filterObj.name !== null) {
+        filterArr = filterArr.filter((product, index) => (
+            product.name.toLowerCase().includes(filterObj.name.toLowerCase())
+        ))
+    }
+
+
+    // cheack minPrice & maxPrice
+    if (filterObj.minPrice !== null && filterObj.maxPrice !== null) {
+        filterArr = filterArr.filter((product, index) => (
+            Number(filterObj.minPrice) <= Number(product.price) && Number(filterObj.maxPrice) >= Number(product.price)
+        ))
+        // console.log(filterArr)
+
+    }
+
+
+
+    // cheackBrand 
+    if (filterObj.brand !== null) {
+        filterArr = filterArr.filter((product, index) => (
+            product.brand === filterObj.brand
+        ))
+    }
+
+    // cheackColor
+
+    if (filterObj.color !== null) {
+        filterArr = filterArr.filter((product, index) => (
+            product.color.length === filterObj.color.length && product.color.every((currentEle) => {
+                if (filterObj.color.indexOf(currentEle) > -1) {
+                    return (currentEle = filterObj.color[filterObj.color.indexOf(currentEle)])
+                }
+            })
+        ))
+    }
+
+
+    displayData(filterArr);
+
+    // console.log(filterObj)
+    // console.log(filterArr)
+}
